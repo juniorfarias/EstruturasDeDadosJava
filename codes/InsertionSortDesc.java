0757fe9c73285097398007e50fba34dcfa712d6a
repl.it user <replituser@example.com>
2021-02-19 01:26:57 +0000

    Add Insertion Sorst Desc funcionando

--- codes/InsertionSortDesc.java
+++ codes/InsertionSortDesc.java
@@ -1,24 +1,28 @@
 public class InsertionSortDesc {
 	public static void main(String[] args){
-		int[] vetor = {1,9,8,7,6,45,89,3};
+		int[] vetor = {1,9,8,100,7,6,45,1,89,3};
 		int selectedItem;
 
 		for(int k = 0; k < vetor.length; k++){
-			System.out.println(vetor[k]);
+			System.out.println("Item => "+(k+1)+", Valor=> "+vetor[k]);
 		}
-		for(int i = vetor.length - 1; i > 0; i--){
-			int j = i - 1;
-			selectedItem = vetor[i];
-			while(vetor[j]<selectedItem && j<(vetor.length-1)){
-				vetor[j+1] = vetor[j];
-				vetor[j] = selectedItem;
-				j++;
+	
+		for(int j = 1; j < vetor.length;j++){
+			selectedItem = vetor[j];
+			int i = j-1;
+			int aux = i;
+			while(vetor[i]<selectedItem && i>=0){
+				vetor[i+1] = vetor[i];
+				vetor[i] = selectedItem;
+				if(i > 0){
+					i -= 1;
+				}
 			}
 		}
 
 		System.out.println("Array Ordenado Decrecendo: ");
 		for(int l = 0; l < vetor.length; l++){
-			System.out.println(vetor[l]);
+			System.out.println("Item => "+(l+1)+", Valor=> "+vetor[l]);
 		}
 	}
 }
